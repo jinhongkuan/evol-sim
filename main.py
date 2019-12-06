84e1667761b2dcfac8ec8a81a060f4546fddeaad
ImSandwich <kuanjh123@gmail.com>
2019-12-05 21:44:10 -0600

    modularity 2.0

--- main.py
+++ main.py
@@ -7,94 +7,98 @@ import os
 import time
 class MDP_Learner:
 
-    @staticmethod
-    def _normalize(row):
+	@staticmethod
+	def _normalize(row):
 
-        row = abs(row)
-        return row/sum(row)
+		row = abs(row)
+		return row/sum(row)
 
-    def make_matrix_consistent(self):
-        self.matrix = np.concatenate((self.matrix[:,0:1], 
-        abs(self.matrix[:,1:2]/sum(self.matrix[:,1:2])),
-        np.apply_along_axis(MDP_Learner._normalize, 1, self.matrix[:,2:self.state_size+2]),
-        np.apply_along_axis(MDP_Learner._normalize, 1, self.matrix[:,self.state_size+2:])), axis=1) 
+	def make_matrix_consistent(self):
+		self.matrix = np.concatenate((self.matrix[:,0:1], 
+		abs(self.matrix[:,1:2]/sum(self.matrix[:,1:2])),
+		np.apply_along_axis(MDP_Learner._normalize, 1, self.matrix[:,2:self.state_size+2]),
+		np.apply_along_axis(MDP_Learner._normalize, 1, self.matrix[:,self.state_size+2:])), axis=1) 
 
-    def __init__(self, state_size, set_strategy=None):
-        # This matrix is a Nx(2N+1) representation of learning strategy 
-        # Each row represents a decision node, expressed in: action, trans. prob | opp. cooperate | opp. defect
-        if set_strategy is None:
-            self.matrix = np.concatenate(([[i%2] for i in range(state_size)],
-            [[1/state_size] for i in range(state_size)],
-            np.random.rand(state_size, state_size),
-            np.random.rand(state_size, state_size)), axis=1)
-        else:
-            self.matrix = set_strategy
+	def __init__(self, state_size, set_strategy=None):
+		# This matrix is a Nx(2N+1) representation of learning strategy 
+		# Each row represents a decision node, expressed in: action, trans. prob | opp. cooperate | opp. defect
+		if set_strategy is None:
+			self.matrix = np.concatenate(([[i%2] for i in range(state_size)],
+			[[1/state_size] for i in range(state_size)],
+			np.random.rand(state_size, state_size),
+			np.random.rand(state_size, state_size)), axis=1)
+		else:
+			self.matrix = set_strategy
 
-        
-        self.state_size = state_size
-        self.make_matrix_consistent()
-        self.state_index = np.random.choice(a=self.matrix.shape[0], p=self.matrix[:,1])
-        self.prev_state_observation = (0,1)
-        self.age = 0
-        
-    def refresh(self):
-        self.state_index = np.random.choice(a=self.matrix.shape[0], p=self.matrix[:,1])
+		
+		self.state_size = state_size
+		self.make_matrix_consistent()
+		self.state_index = np.random.choice(a=self.matrix.shape[0], p=self.matrix[:,1])
+		self.prev_state_observation = (0,1)
+		self.age = 0
+		
+	def refresh(self):
+		self.state_index = np.random.choice(a=self.matrix.shape[0], p=self.matrix[:,1])
 
-    def play(self):
-        return self.matrix[self.state_index,0]
+	def play(self):
+		return self.matrix[self.state_index,0]
 
-    def observe(self, observation):
-        if observation is not None:
-            if observation == 0:
-                trans_prob = self.matrix[self.state_index, 2:self.state_size+2]
-            else:
-                trans_prob = self.matrix[self.state_index, self.state_size+2:]
-    
-            next_state = np.random.choice(range(self.state_size),p=trans_prob)
-            self.prev_state_observation = (self.state_index, int(next_state+2+observation*self.state_size))
-            self.state_index = next_state
+	def observe(self, observation):
+		if observation is not None:
+			if observation == 0:
+				trans_prob = self.matrix[self.state_index, 2:self.state_size+2]
+			else:
+				trans_prob = self.matrix[self.state_index, self.state_size+2:]
+	
+			next_state = np.random.choice(range(self.state_size),p=trans_prob)
+			self.prev_state_observation = (self.state_index, int(next_state+2+observation*self.state_size))
+			self.state_index = next_state
 
-    
+	
 
 
-    def mutate(self, rate):
-        # Mutate mixed strategy
-        self.matrix[:,1] += Mutation.pairwise_gaussian(rate, self.matrix[:,1])
-        
-        # Mutate transition probabilities 
-        for i in range(self.matrix.shape[0]):
-            for obs_shift in range(2): # Do calculation separately for the Obs(Coop) and Ops(Defect) sets of columns
-                shift = 2 + obs_shift * self.state_size
-                mutation = Mutation.pairwise_gaussian(rate,self.matrix[i,shift:shift+self.state_size])
-                self.matrix[i,shift:shift+self.state_size] += mutation
+	def mutate(self, rate):
+		# Mutate mixed strategy
+		self.matrix[:,1] += Mutation.pairwise_gaussian(rate, self.matrix[:,1])
+		
+		# Mutate transition probabilities 
+		for i in range(self.matrix.shape[0]):
+			for obs_shift in range(2): # Do calculation separately for the Obs(Coop) and Ops(Defect) sets of columns
+				shift = 2 + obs_shift * self.state_size
+				mutation = Mutation.pairwise_gaussian(rate,self.matrix[i,shift:shift+self.state_size])
+				self.matrix[i,shift:shift+self.state_size] += mutation
 
-        self.make_matrix_consistent() # Only to fix floating-point error
+		self.make_matrix_consistent() # Only to fix floating-point error
 
 class Mutation:
-    @staticmethod
-    def pairwise_gaussian(rate, array):
-        normal_values = np.zeros(len(array))
-        projected = copy(array)
-        for i in range(len(projected)):
-            j = (i+1) % len(projected)
-            alpha = np.random.normal(loc=0, scale=rate)
-            normal_values[i] += alpha 
-            normal_values[j] -= alpha  
-            projected[i] += alpha 
-            projected[j] -= alpha 
-        min_scaling = 1.0
-        for i in range(len(normal_values)):
-            i_after = projected[i]
-            excess = i_after - 1 if i_after > 1 else abs(min(0, i_after))
-            scaling = 1 - excess/abs(normal_values[i])
-            min_scaling = min(min_scaling, scaling)
-        normal_values *= min_scaling
+	@staticmethod
+	def pairwise_gaussian(rate, array):
+		normal_values = np.zeros(len(array))
+		projected = copy(array)
+		for i in range(len(projected)):
+			j = (i+1) % len(projected)
+			alpha = np.random.normal(loc=0, scale=rate)
+			normal_values[i] += alpha 
+			normal_values[j] -= alpha  
+			projected[i] += alpha 
+			projected[j] -= alpha 
+		min_scaling = 1.0
+		for i in range(len(normal_values)):
+			i_after = projected[i]
+			excess = i_after - 1 if i_after > 1 else abs(min(0, i_after))
+			scaling = 1 - excess/abs(normal_values[i])
+			min_scaling = min(min_scaling, scaling)
+		normal_values *= min_scaling
 
-        return normal_values
+		return normal_values
 
 class Selection:
 	@staticmethod
-	def truncation(population, scores, cutoff):
+	def truncation(kwargs):
+		population = kwargs["population"]
+		scores = kwargs["scores"]
+		cutoff = kwargs["cutoff"]
+
 		# Kill off half and clone the rest 
 		new_pops = sorted([(scores[i], population[i]) for i in range(len(scores))], key=lambda x:x[0], reverse=True) 
 		new_pops = new_pops[:cutoff]
@@ -102,83 +106,114 @@ class Selection:
 		return new_pops
 
 	@staticmethod
-	def uniform_random(population, cutoff):
+	def uniform_random(kwargs):
+		population = kwargs["population"]
+		cutoff = kwargs["cutoff"]
+
 		return np.random.choice(population, size=cutoff)
 
+	@staticmethod 
+	def get_selection(command, **kwargs):
+		if command == "truncation":
+			return Selection.truncation(kwargs)
+		elif command == "uniform_random":
+			return Selection.uniform_random(kwargs)
+
 class Interaction:
+
 	@staticmethod
-	def pairwise(population, fresh_mind, payoff_matrix):
+	def pairwise(kwargs):
+		population = kwargs["population"]
+		fresh_mind = kwargs["fresh_mind"]
+		payoff_matrix = kwargs["payoff_matrix"]
+
 		scores = [0]*len(population)
 		combinations = copy(template_combinations)
 		for i in range(len(population)):
-		    for j in range(i+1, len(population)):
-		        for k in range(repetition):
-		            i_play = int(population[i].play())
-		            j_play = int(population[j].play())
-		            population[i].observe(j_play)
-		            population[j].observe(i_play)
-		            scores[i] += payoff_matrix[i_play,j_play,0]
-		            scores[j] += payoff_matrix[i_play,j_play,1]
-		            play = play_to_str[i_play+j_play]
-		            if play not in combinations:
-		                combinations[play] = 0 
-		            combinations[play] += 1
-		        if fresh_mind:
-		            population[i].refresh()
-		            population[j].refresh()
+			for j in range(i+1, len(population)):
+				for k in range(repetition):
+					i_play = int(population[i].play())
+					j_play = int(population[j].play())
+					population[i].observe(j_play)
+					population[j].observe(i_play)
+					scores[i] += payoff_matrix[i_play,j_play,0]
+					scores[j] += payoff_matrix[i_play,j_play,1]
+					play = play_to_str[i_play+j_play]
+					if play not in combinations:
+						combinations[play] = 0 
+					combinations[play] += 1
+				if fresh_mind:
+					population[i].refresh()
+					population[j].refresh()
 		return scores, combinations
 
+	@staticmethod
+	def get_interaction(command, **kwargs):
+		if command == "pairwise":
+			return Interaction.pairwise(kwargs)
    
 
 class Population:
-    def __init__(self, population_size, state_size, repetition, fresh_mind = False, set_strategy=None):
-        self.pops = [MDP_Learner(state_size, set_strategy) for i in range(population_size)]
-        self.repetition = repetition 
-        self.fresh_mind = fresh_mind
-        self.population_size = population_size
+	def __init__(self, population_size, state_size, repetition, fresh_mind = False, set_strategy=None):
+		self.pops = [MDP_Learner(state_size, set_strategy) for i in range(population_size)]
+		self.repetition = repetition 
+		self.fresh_mind = fresh_mind
+		self.population_size = population_size
 
-    def aggregate_selection(self, payoff_matrix):
-        num_offsprings = self.population_size//2
+	def aggregate_selection(self, payoff_matrix_, interaction, parent_sel, survivor_sel, overlap, num_offsprings):
 
-        np.random.shuffle(self.pops)
-        for pop in self.pops:
-            pop.age += 1
+		np.random.shuffle(self.pops)
+		for pop in self.pops:
+			pop.age += 1
 
-        
+		parent_scores, combinations = Interaction.get_interaction(interaction, 
+			population=self.pops, 
+			fresh_mind=self.fresh_mind, 
+			payoff_matrix=payoff_matrix_)
 
-        scores, combinations = Interaction.pairwise(self.pops, self.fresh_mind, payoff_matrix)
-        parents = Selection.truncation(self.pops, scores, self.population_size - num_offsprings)
-        survivors = parents 
+		parents = Selection.get_selection(parent_sel, 
+			population=self.pops, 
+			scores=parent_scores, 
+			cutoff=num_offsprings)
 
-        offsprings = []
-        for parent in parents:
-            offsprings += [deepcopy(parent)]
-            offsprings[-1].age = 0
-            offsprings[-1].mutate(0.02) 
+		offsprings = []
+		for parent in parents:
+			offsprings += [deepcopy(parent)]
+			offsprings[-1].age = 0
+			offsprings[-1].mutate(0.02) 
 
+		if overlap:
+			self.pops += offsprings
+		'''
+		This is temporarily commented out for speed (since num(parent)==num(offsprings) we can skip this computation)
+		scores_, _ = Interaction.get_interaction(interaction, 
+			population=self.pops, 
+			fresh_mind=self.fresh_mind, 
+			payoff_matrix=payoff_matrix_)
 
-        overlapping = False 
-        if overlapping:
-            self.pops += offsprings
-            scores, combinations = Interaction.pairwise(self.pops, self.fresh_mind, payoff_matrix)
-            self.pops = Selection.truncation(self.pops, scores, self.population_size)
-        else:
-            self.pops = survivors + offsprings
+		self.pops = Selection.get_selection(survivor_sel, 
+			population=self.pops, 
+			scores=scores_, 
+			cutoff=self.population_size - 0 if overlap else num_offsprings)
+		'''
+		if not overlap:
+			self.pops = parents
+			self.pops += offsprings
 
-        
-        return combinations
+		
+		return combinations
 
 rand_seed = time.time()
 random.seed(rand_seed)
 # Change these
 name = "data/pd_test"
 prisoner = [[[1,4],[3,3]],
-                [[2,2],[4,1]]]
+				[[2,2],[4,1]]]
 
 no_conflict = [[[3,2],[4,4]],
-                [[1,1],[2,3]]]
+				[[1,1],[2,3]]]
 p_matrix = prisoner
-iterations = 4000
+iterations = 10000
 
 window = 25
 repetition = 10
@@ -194,22 +229,30 @@ tally = {"CC":[], "C/D":[], "DD":[]}
 test_gene_coverage = []
 fossils = []
 for i in range(iterations):
-    
-    for pop in Pops.pops:
-        test_gene_coverage += [pop.matrix[0,2]] 
-    combinations = (Pops.aggregate_selection(np.asarray([[p_matrix[0][1],p_matrix[0][0]],
-                                                        [p_matrix[1][1],p_matrix[1][0]]])))
-    for key in combinations:
-        tally[key] += [combinations[key]]
-    if i % window == 0:
-        print(i)
-        all_matrices = []
-        for pop in Pops.pops:
-            all_matrices += copy([pop.matrix])
-        fossils += [all_matrices]
+	
+	for pop in Pops.pops:
+		test_gene_coverage += [pop.matrix[0,2]] 
+
+	combinations = Pops.aggregate_selection(
+		payoff_matrix_= np.asarray([[p_matrix[0][1],p_matrix[0][0]],
+						[p_matrix[1][1],p_matrix[1][0]]]),
+		interaction = "pairwise",
+		parent_sel = "truncation",
+		survivor_sel = "truncation",
+		overlap = False,
+		num_offsprings = len(Pops.pops)//2)
+
+	for key in combinations:
+		tally[key] += [combinations[key]]
+	if i % window == 0:
+		print(i)
+		all_matrices = []
+		for pop in Pops.pops:
+			all_matrices += copy([pop.matrix])
+		fossils += [all_matrices]
 
 for key in tally:
-    plt.plot([sum(tally[key][i*window:(i+1)*window])/window for i in range(iterations//window)])
+	plt.plot([sum(tally[key][i*window:(i+1)*window])/window for i in range(iterations//window)])
 
  
 
@@ -219,20 +262,20 @@ plt.savefig(fname=name+".png")
 plt.show()
 
 with open(name + ".csv", "w", newline="") as f:
-    f_writer = csv.writer(f)
-    f_writer.writerow(["Seed", rand_seed])
-    f_writer.writerow(["Action", "Start. P", "C|C", "C|D", ])
-    for i in range(len(Pops.pops)):
-        mat = Pops.pops[i].matrix
-        for r in range(mat.shape[0]):
-            for c in range(1,mat.shape[1]):
-                mat[r,c] = round(mat[r,c]*100)/100
-        f_writer.writerows(mat)
-        f_writer.writerow([])
-        
+	f_writer = csv.writer(f)
+	f_writer.writerow(["Seed", rand_seed])
+	f_writer.writerow(["Action", "Start. P", "C|C", "C|D", ])
+	for i in range(len(Pops.pops)):
+		mat = Pops.pops[i].matrix
+		for r in range(mat.shape[0]):
+			for c in range(1,mat.shape[1]):
+				mat[r,c] = round(mat[r,c]*100)/100
+		f_writer.writerows(mat)
+		f_writer.writerow([])
+		
 while True:
-    generation = int(input("Generation to peek: "))
-    for i, pop_matrix in enumerate(fossils[generation]):
-        print("--- Agent {0} ---".format(i))
-        for row in pop_matrix:
-            print(row)
+	generation = int(input("Generation to peek: "))
+	for i, pop_matrix in enumerate(fossils[generation]):
+		print("--- Agent {0} ---".format(i))
+		for row in pop_matrix:
+			print(row)
